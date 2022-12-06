f11f9a5bd87ed1055686c1a4e5ad863f525551cb
pablo-chacon <Snemilio@protonmail.com>
2022-12-06 14:27:44 +0100

    .

--- Hangman1/src/main/java/com/example/hangman1/Application.java
+++ Hangman1/src/main/java/com/example/hangman1/Application.java
@@ -1,22 +1,88 @@
 package com.example.hangman1;
 
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage)
+    {
+
+        try {
+
+            // set title for the stage
+            stage.setTitle("Jail Man");
+
+            // create a label
+            Label chars = new Label("_ _ _ _ _ _ _ _");
+            Label alphabet  = new Label("A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z");
+            // create a text field
+            TextField textfield = new TextField("Input: ");
+
+            // set preferred column count
+            textfield.setPrefColumnCount(10);
+
+            // create a button
+            Button button = new Button("Guess!");
+
+            // add the label, text field and button
+            VBox vbox = new VBox(chars, alphabet, textfield, button);
+
+            // set spacing
+            vbox.setSpacing(10);
+
+            // set alignment for the HBox
+            vbox.setAlignment(Pos.BOTTOM_CENTER);
+
+
+            // create a scene
+            Scene scene = new Scene(vbox, 860, 520);
+
+            // create a input stream
+            FileInputStream input = new FileInputStream("src/main/resources/com/example/testjfx/Prison-jail.png");
+
+            // create a image
+            Image image = new Image(input);
+
+            // create a background image
+            BackgroundImage backgroundimage = new BackgroundImage(image,
+                    BackgroundRepeat.NO_REPEAT,
+                    BackgroundRepeat.NO_REPEAT,
+                    BackgroundPosition.CENTER,
+                    BackgroundSize.DEFAULT);
+
+            // create Background
+            Background background = new Background(backgroundimage);
+
+            // set background
+            vbox.setBackground(background);
+
+            // set the scene
+            stage.setScene(scene);
+
+            stage.show();
+        }
+
+        catch (Exception e) {
+
+            System.out.println(e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
         launch();
     }
+
+
+
 }
--- Hangman1/src/main/java/com/example/hangman1/HelloController.java
+++ Hangman1/src/main/java/com/example/hangman1/HelloController.java
@@ -2,13 +2,16 @@ package com.example.hangman1;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BackgroundImage;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-    // I pushed stuff
+
+    @FXML
+    public BackgroundImage background;
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
     }
 }

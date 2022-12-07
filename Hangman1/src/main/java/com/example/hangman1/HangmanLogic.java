b9825ae07c91761ef3106ae69c3a81b1739ac550
Leila Amrayi <leilaamraie@yahoo.com>
2022-12-07 22:14:32 +0100

    Added VirtualKeyboard and HangmanLogic

---
+++ Hangman1/src/main/java/com/example/hangman1/HangmanLogic.java
@@ -0,0 +1,107 @@
+package com.example.hangman1;
+
+import java.util.*;
+
+public class HangmanLogic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean userWon = false;
+        final char dash = '-';
+
+        System.out.println("Welcome to game!");
+        List<String> secretWordList = new ArrayList<>(Arrays.asList("sun", "goal", "weather", "health", "happiness", "hi"));
+        Random random = new Random();
+        int index = random.nextInt(secretWordList.size() - 1);
+        String secretWord = secretWordList.get(index);
+        char[] secretWordArray = new char[secretWord.length()];
+        for (int i = 0; i < secretWord.length(); i++) {
+            secretWordArray[i] = dash;
+        }
+
+        System.out.println("secretWord is " + secretWord);
+
+        printWordArray(secretWordArray);
+
+        List<String> correctGuesses = new ArrayList<>();
+        List<String> wrongGuesses = new ArrayList<>();
+        int maxChances = 8;
+        for (int guessAttempt = 1; guessAttempt <= maxChances; guessAttempt++) {
+            System.out.println("\r\nChance " + guessAttempt + "/" + maxChances + ", Guess a letter: ");
+            String input = scanner.next();
+            System.out.println("user input is: "+input);
+
+            if (input == null || "".equals(input.trim())) {
+                System.out.println("Invalid input. Please try again.");
+                continue;
+            }
+
+            if (correctGuesses.contains(input) || wrongGuesses.contains(input)) {
+                System.out.println("You guessed this letter before. Try another one!");
+                guessAttempt--; // don't count this duplicate letter
+                continue; // don't do anything and just loop again
+            }
+
+            // if user guessed the whole word in one go, set the flag to true and skip the loop
+            if (secretWord.equals(input)) {
+                userWon = true;
+                break;
+            }
+
+
+            if (secretWord.contains(input)) {
+                // count the guess as correct only if it's one character
+                if (input.length() == 1) {
+                    correctGuesses.add(input);
+                } else {
+                    // if it's multiple characters, then it's not a correct guess even if it's part of the secret word
+                    wrongGuesses.add(input);
+                }
+            } else {
+                wrongGuesses.add(input);
+            }
+
+            // show incorrect guessed letters so far
+            //System.out.println("Wrong guesses so far: " + String.join(',', wrongGuesses.ToArray()));
+
+            // Solution #2
+            // process the guesses and keep the results as a word.
+            for (String guess : correctGuesses) {
+                char guessedChar = guess.charAt(0);
+                int i = 0;
+                for (char secretLetter : secretWord.toCharArray()) {
+                    if (secretLetter == guessedChar) {
+                        secretWordArray[i] = secretLetter;
+                    }
+                    i++;
+                }
+            }
+
+            // present the word
+            printWordArray(secretWordArray);
+
+            // if no dash is left in the word, it means all letters are revealed and user wins
+            if (!String.valueOf(secretWordArray).contains(String.valueOf(dash)))
+            {
+                userWon = true;
+                break;
+            }
+
+        }
+
+        if (userWon) {
+            System.out.println("\r\nCongrats! You Won!");
+        } else {
+            System.out.println("\r\nSorry! You did not win. The correct word is: \r\n" + secretWord);
+        }
+
+    }
+
+    private static void printWordArray(char[] secretWordArray){
+        System.out.println("Word: ");
+        for (char ch : secretWordArray) {
+            System.out.print(ch + " ");
+        }
+    }
+
+}
+

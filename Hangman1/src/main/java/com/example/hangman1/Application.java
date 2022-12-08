3f02dd1efff8526c64a9157d01684722ccc0f526
divby0exc <seddigh.daniel@gmail.com>
2022-12-08 18:28:19 +0100

    Seperated scenes and built scenes for respective buttons

--- Hangman1/src/main/java/com/example/hangman1/Application.java
+++ Hangman1/src/main/java/com/example/hangman1/Application.java
@@ -1,73 +1,28 @@
 package com.example.hangman1;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
-import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.security.Key;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Application extends javafx.application.Application {
-    private Keyboard key = new Keyboard();
-    private Alphabet al = new Alphabet();
-    private MouseClickButtons mcb = new MouseClickButtons();
+    SceneChoice sceneChoice = new SceneChoice();
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("HANGMAN");
-        
-        GridPane pane = new GridPane();
-        Button[] rowOne = al.returnAndSetBtnName("one");
-        Button[] rowTwo = al.returnAndSetBtnName("two");
-        Button[] rowThree = al.returnAndSetBtnName("three");
 
-        mcb.keyListener(rowOne);
-        mcb.keyListener(rowTwo);
-        mcb.keyListener(rowThree);
+        //Start menu
+        //Scene mainGame = sceneChoice.mainGame();
 
-        Line firstLine = new Line(727,697,727,194);
-        pane.getChildren().add(firstLine);
+        // Mode
+        Scene gameMenu = sceneChoice.gameMenu(stage);
+
+        // How to play
+        Scene howToPlay = sceneChoice.howToPlay(stage);
 
 
-
-        pane.addRow(0, rowOne);
-        pane.addRow(1, rowTwo);
-        pane.addRow(2, rowThree);
-        Image img = new Image("C:\\Users\\DefconK1ll4\\Desktop\\Hangman\\Hangman1\\src\\main\\java\\com\\example\\hangman1\\background-prison-cell.jpg");
-        BackgroundImage bgi = new BackgroundImage(img,BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT,BackgroundPosition.CENTER,BackgroundSize.DEFAULT);
-        Background bg = new Background(bgi);
-        pane.setBackground(bg);
-        stage.setMaximized(true);
-        Scene scene = new Scene(pane);
-
-        scene.setOnMouseClicked((event) -> {
-            System.out.println("X " + event.getX() + " Y " + event.getY());
-        });
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent kev) {
-                key.getKeyPressed(kev);
-            }
-        });
-
-
-            stage.setScene(scene);
+            stage.setScene(gameMenu);
             stage.show();
-
     }
 
     public static void main(String[] args) {
---
+++ Hangman1/src/main/java/com/example/hangman1/SceneChoice.java
@@ -0,0 +1,124 @@
+package com.example.hangman1;
+
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class SceneChoice {
+    private Alphabet al = new Alphabet();
+    private MouseClickButtons mcb = new MouseClickButtons();
+    private Keyboard key = new Keyboard();
+    private javafx.scene.Scene whichScene;
+    private String howToPlayMultiplayer = "Du ska komma på ett ord för den andre spelarens gubbe. Den andre spelaren ska försöka gissa ordet genom att trycka på en bokstav i taget. Det går såklart bra att även trycka på tangenterna också. ";
+    private String howToPlaySingleplayer = "Du får ett ord av datorn som du ska gissa. Du kan trycka på bokstäverna på skärmen eller på tangentbordet. ";
+    private String howToPlayEnd = "Du har 10 försök på dig. Du ser när strecken ritas på skärmen men även försöken ovanför bokstäverna så du slipper komma ihåg allt.";
+    private String howToPlayInteractive = "I detta läge kan du bestämma själv hur många streck som ska ritas. Det betyder att ni bestämmer själva om det ska gå fort eller om ni vill rita mer streck så spelet blir längre. När gubben är hängd så är spelet över. ";
+
+
+    public javafx.scene.Scene mainGame() {
+        GridPane pane = new GridPane();
+        Button[] rowOne = al.returnAndSetBtnName("one");
+        Button[] rowTwo = al.returnAndSetBtnName("two");
+        Button[] rowThree = al.returnAndSetBtnName("three");
+
+        mcb.keyListener(rowOne);
+        mcb.keyListener(rowTwo);
+        mcb.keyListener(rowThree);
+
+        pane.addRow(0, rowOne);
+        pane.addRow(1, rowTwo);
+        pane.addRow(2, rowThree);
+        Image img = new Image("C:\\Users\\DefconK1ll4\\Desktop\\Hangman\\Hangman1\\src\\main\\java\\com\\example\\hangman1\\background-prison-cell.jpg");
+        BackgroundImage bgi = new BackgroundImage(img, BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+        Background bg = new Background(bgi);
+        pane.setBackground(bg);
+        javafx.scene.Scene mainGameScene = new javafx.scene.Scene(pane);
+
+        //Checking pixels to decide positions on the lines
+        mainGameScene.setOnMouseClicked((event) -> {
+            System.out.println("X " + event.getX() + " Y " + event.getY());
+        });
+        // Keyboard listener
+        mainGameScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent kev) {
+                key.getKeyPressed(kev);
+            }
+        });
+
+        return mainGameScene;
+    }
+    public javafx.scene.Scene gameMenu(Stage stage) {
+        Canvas canvas = new Canvas(150,150);
+        canvas.getGraphicsContext2D();
+        VBox vBox = new VBox(canvas);
+        Button playGame = new Button("SPELA!");
+        playGame.setOnAction(e -> stage.setScene(mainGame()));
+        Button mode = new Button("Svårighetsgrad");
+        //mode.setOnAction(e -> stage.setScene(sceneArr[]));
+        Button help = new Button("Hur spelar jag?");
+        help.setOnAction(e -> stage.setScene(howToPlay(stage)));
+        vBox.getChildren().addAll(playGame,mode,help);
+        javafx.scene.Scene gameMenuScene = new javafx.scene.Scene(vBox);
+
+        return gameMenuScene;
+    }
+    public javafx.scene.Scene howToPlay(Stage stage) {
+        Canvas canvas1 = new Canvas(150,150);
+        canvas1.getGraphicsContext2D();
+        HBox hBox = new HBox(canvas1);
+        Button singleplayer = new Button("En person");
+        singleplayer.setOnAction(e -> stage.setScene(howToPlay(stage, "en")));
+        Button multiplayer = new Button("Flera spelare");
+        multiplayer.setOnAction(e -> stage.setScene(howToPlay(stage, "multi")));
+        Button interactive = new Button("Rita själv");
+        interactive.setOnAction(e -> stage.setScene(howToPlay(stage, "inter")));
+        Button previous = new Button("Gå tillbaka");
+        previous.setOnAction(e -> stage.setScene(gameMenu(stage)));
+        hBox.getChildren().addAll(singleplayer,multiplayer,interactive,previous);
+        javafx.scene.Scene howToPlayScene = new javafx.scene.Scene(hBox);
+
+        return howToPlayScene;
+    }
+    public javafx.scene.Scene howToPlay(Stage stage, String helpPlayers) {
+        javafx.scene.Scene helpPlay = null;
+        if(helpPlayers.equalsIgnoreCase("en")) {
+            Canvas canvas = new Canvas();
+            canvas.getGraphicsContext2D();
+            HBox hBox = new HBox(canvas);
+            Label label = new Label();
+            Button previous = new Button("Gå tillbaka");
+            previous.setOnAction(e -> stage.setScene(howToPlay(stage)));
+            hBox.getChildren().addAll(label, previous);
+            helpPlay = new javafx.scene.Scene(hBox);
+        } else if(helpPlayers.equalsIgnoreCase("multi")) {
+            Canvas canvas = new Canvas();
+            canvas.getGraphicsContext2D();
+            HBox hBox = new HBox(canvas);
+            Label label = new Label();
+            Button previous = new Button("Gå tillbaka");
+            previous.setOnAction(e -> stage.setScene(howToPlay(stage)));
+            hBox.getChildren().addAll(label, previous);
+            helpPlay = new javafx.scene.Scene(hBox);
+        } else if(helpPlayers.equalsIgnoreCase("inter")) {
+            Canvas canvas = new Canvas();
+            canvas.getGraphicsContext2D();
+            HBox hBox = new HBox(canvas);
+            Label label = new Label();
+            Button previous = new Button("Gå tillbaka");
+            previous.setOnAction(e -> stage.setScene(howToPlay(stage)));
+            hBox.getChildren().addAll(label, previous);
+            helpPlay = new javafx.scene.Scene(hBox);
+        }
+        return helpPlay;
+    }
+
+    //Skapa 3 olika scener för help selection
+    //Skapa 3 olika modes såklart
+}

674b335feae6ec620569796f4459463e2901eba9
Leila Amrayi <leilaamraie@yahoo.com>
2022-12-10 02:11:11 +0100

    update virtual keyboard

--- Hangman1/src/main/java/com/example/hangman1/VirtualKeyboard.java
+++ Hangman1/src/main/java/com/example/hangman1/VirtualKeyboard.java
@@ -1,12 +1,19 @@
 package com.example.hangman1;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,6 +21,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class VirtualKeyboard extends Application {
+
+    String secretWordInDash;
+    String secretWord;
+    String alph;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -27,27 +38,65 @@ public class VirtualKeyboard extends Application {
         HBox row2=creatRow(letters2);
         row2.setPadding(new Insets(70, 70,10,10));
 
+
         Group root = new Group();
         root.getChildren().add(row);
         root.getChildren().add(row1);
         row2.setMinWidth(100);
         root.getChildren().add(row2);
 
-        Scene scene = new Scene(root, 400, 100, Color.BEIGE);
+
+        Label secWoDash = new Label(secretWordInDash);
+        secWoDash.setFont(new Font("Arial", 25));
+        /*secWoDash.setBorder(new Border(new BorderStroke(Color.valueOf("#9E9E9E"),
+                BorderStrokeStyle.SOLID,
+                CornerRadii.EMPTY,
+                BorderWidths.DEFAULT)));*/
+
+        PasswordField ownWord = new PasswordField();
+        Label word = new Label("Enter your word:");
+        word.setFont(new Font("Arial", 12));
+        word.setTextAlignment(TextAlignment.CENTER);
+        Button button = new Button("OK");
+        button.setOnAction(action -> {
+            secretWord=(ownWord.getText());
+            System.out.print(secretWord);
+            secretWordInDash = createDashedWord(secretWord);
+            secWoDash.setText(secretWordInDash);
+        });
+        HBox hbox = new HBox(word,ownWord,button,secWoDash);
+        hbox.setPadding(new Insets(100, 100,10,10));
+        root.getChildren().add(hbox);
+        hbox.setSpacing(10);
+
+
+        Scene scene = new Scene(root, 600, 200, Color.BEIGE);
         stage.setTitle("Keyboard");
         stage.setScene(scene);
         stage.show();
 
     }
     public HBox creatRow(List<String> letters){
+
         HBox hBox = new HBox();
         for (String letter : letters){
             Button button = new Button(letter);
             button.setStyle("-fx-text-fill: #0000ff");
+            button.setOnAction(action -> {
+                  alph=(button.getText());
+                  System.out.print(alph);
+
+            });
             hBox.getChildren().add(button);
         }
         return hBox;
     }
+    public String createDashedWord(String word) {
+        final String dash = "- ";
+        int count = word.length();
+        return dash.repeat(count);
+    }
+
 
     public static void main(String[] args) {
         launch();

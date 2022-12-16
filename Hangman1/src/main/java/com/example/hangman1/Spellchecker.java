ef81202086bb6ca93c8766b85c9e9245b91f0114
Leila Amrayi <leilaamraie@yahoo.com>
2022-12-16 15:00:27 +0100

    correct spellingchecker file

--- Hangman1/src/main/java/com/example/hangman1/Spellchecker.java
+++ Hangman1/src/main/java/com/example/hangman1/Spellchecker.java
@@ -4,20 +4,30 @@ package com.example.hangman1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Scanner;
 
 
 public class Spellchecker {
     public static boolean SpellCheck(String word, String language) {
 
-        File swedishDictionary = new File("src/main/resources/swedish-dictionary.txt");
-        File englishDictionary = new File("src/main/resources/english-dictionary.txt");
+        URL urlSE = Spellchecker.class.getClassLoader().getResource("swedish-dictionary.txt");
+        URL urlEN = Spellchecker.class.getClassLoader().getResource("english-dictionary.txt");
 
+        File swedishDictionary ;
+        File englishDictionary ;
+        try {
+            swedishDictionary = new File(urlSE.toURI());
+            englishDictionary = new File(urlEN.toURI());
 
-
-
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        
 
         boolean isFound = false;
+
 
         if(language.equalsIgnoreCase("Swedish")) {
             try {
